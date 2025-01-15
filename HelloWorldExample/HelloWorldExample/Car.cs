ef0ce82484299a14023586e5e4f5d441af772feb
bclayson23 <156945138+bclayson23@users.noreply.github.com>
2025-01-14 19:45:19 -0500

    First version of Hello World
    a car object and a form

---
+++ HelloWorldExample/HelloWorldExample/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldExample
+{
+    public class Car
+    {
+        public string make;
+        public string model;
+        public int year;
+        public string VIN;
+        public string exteriorColor;
+        public string interiorColor;
+        public Car(string make, string model, int year, string vIN, string exteriorColor, string interiorColor)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.VIN = vIN;
+            this.exteriorColor = exteriorColor;
+            this.interiorColor = interiorColor;
+        }
+    }
+}
